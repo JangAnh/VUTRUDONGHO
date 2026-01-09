c592c65aadceb96a6180f4b01decf06b23bb062c
JangAnh <janganh2004@gmail.com>
2026-01-09 07:31:46 +0700

    thêm google analytics

--- vutrudongho/components/header.php
+++ vutrudongho/components/header.php
@@ -96,3 +96,13 @@ if (isAdminLogged()) {
     ');
 }
 ?>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4R6K6CXCHC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-4R6K6CXCHC');
+</script>
+<!-- End Google tag -->
